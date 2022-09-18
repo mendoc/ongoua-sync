5a0366c0495d35286c35652918271cacf33af44f
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2022-09-19 00:11:46 +0100

    Copie les depots prives

--- OngouaSync.php
+++ OngouaSync.php
@@ -4,7 +4,7 @@
  * Ce script permet de synchroniser un dépôt Github et 
  * un dossier distant héberger dans un serveur supportant PHP
  * 
- * A chaque push sur la branche main, il récupère les fichiers sur Github et les met
+ * A chaque push sur une branche, il récupère les fichiers sur Github et les met
  * à jour dans le dossier où il se trouve.
  * 
  * @author Dimitri ONGOUA
@@ -12,22 +12,39 @@
 
 /**
  * =========================================================
- * SECURITE
+ * SECURITE : Cle de synchronisation
  * 
  * Définition d'une clé pour éviter que des clients non autorisés
- * n'exécute le script
+ * n'exécutent le script
  * =========================================================
  */
 define('ONGOUA_SYNC_KEY', "OngouaSync");
 
 /**
  * =========================================================
+ * SECURITE : Personal Access Token
+ * 
+ * Définition d'un jeton permettant de copier un depot prive
+ * La creation de jeton se fait dans les parametres du compte Github
+ * Settings > Developer settings > Personal access tokens
+ * =========================================================
+ */
+define('ONGOUA_PERSONAL_ACCESS_TOKEN', "");
+
+/**
+ * =========================================================
  * BRANCHE
  * 
  * Définition de la branche à surveiller
+ * Exemples :
+ * define('ONGOUA_BRANCH', "main");
+ * define('ONGOUA_BRANCH', "master");
+ * 
+ * Si auncune branche n'est precisee, 
+ * la branche par defaut du depot sera consideree
  * =========================================================
  */
-define('ONGOUA_BRANCH', "main");
+define('ONGOUA_BRANCH', "");
 
 /**
  * =========================================================
@@ -76,11 +93,17 @@ $evenement = isset($headers["X-GitHub-Event"]) ? $headers["X-GitHub-Event"] : $h
 // Si ce n'est pas un push on ne continue pas.
 if ($evenement !== 'push') die("Evènement ($evenement) non pris en charge.");
 
-$infos = json_decode($payload, TRUE);
-$depot = $infos["repository"]["full_name"];
+// Si une branche a ete precisee pour surveillance
+$infos      = json_decode($payload, TRUE);
+$branche    = $infos["repository"]["default_branch"];
+$depot      = $infos["repository"]["full_name"];
+$visibility = $infos["repository"]["visibility"];
+
+if (strlen(ONGOUA_BRANCH) > 0)
+    if ($branche !== ONGOUA_BRANCH) die("Les modifications de cette branche ($branche) sont ignorées.");
 
 // Synchronisation du dossier
-OngouaSync($depot);
+OngouaSync($depot, $visibility, $branche);
 
 /**
  * =========================================================
@@ -88,7 +111,7 @@ OngouaSync($depot);
  * =========================================================
  */
 
-function OngouaSync($depot)
+function OngouaSync($depot, $visibility, $branche)
 {
     if (!strpos($depot, "/")) die("Nom du dépôt incorrect.");
 
@@ -96,13 +119,17 @@ function OngouaSync($depot)
 
     if (count($infos) !== 2) die("Format du dépôt incorrect. Exemple: utilisateur/depot");
 
-    $branche          = ONGOUA_BRANCH;
     $nom_depot        = $infos[1];
     $nom_dossier_temp = "$nom_depot-$branche";
     $nom_fichier_zip  = __DIR__ . DIRECTORY_SEPARATOR . time() . ".zip";
+    $url_depot_prive  = "https://api.github.com/repos/$depot/zipball/$branche";
+    $url_depot_public = "https://github.com/$depot/archive/refs/heads/$branche.zip";
 
     // Récupération des fichiers du dépôt
-    copy("https://github.com/$depot/archive/refs/heads/$branche.zip", $nom_fichier_zip);
+    if ($visibility === "public")
+        copyPublic($url_depot_public, $nom_fichier_zip);
+    else
+        copyPrivate($url_depot_prive, $nom_fichier_zip);
 
     // Récupération du chemin absolu du dossier de travail
     $chemin_dossier_travail = pathinfo($nom_fichier_zip, PATHINFO_DIRNAME);
@@ -150,6 +177,53 @@ function rcopy($src, $dst)
         foreach ($files as $file)
             if ($file != "." && $file != "..") rcopy("$src/$file", "$dst/$file");
     } else if (file_exists($src)) copy($src, $dst);
+}
+
+function copyPublic($url, $dest)
+{
+    return copy($url, $dest);
+}
+
+function copyPrivate($url, $dest)
+{
+    $header = array();
+    $header[] = "Accept: application/vnd.github+json";
+    $header[] = "User-Agent: mendoc-ongoua-sync";
+    $header[] = "Authorization: Bearer " . ONGOUA_PERSONAL_ACCESS_TOKEN;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+    $reponse = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo "Erreur CURL - " . curl_error($ch);
+    } else {
+        $status = curl_getinfo($ch);
+        if ($status["http_code"] == 302) {
+            $depot_down_url = getURL($reponse);
+            if ($depot_down_url === false)
+                echo "Erreur lors de la recuperation de l'URL du depot";
+            else
+                copyPublic($depot_down_url, $dest);
+        } else {
+            echo $status["http_code"] == 200 ? "OK" : "Erreur " . $status["http_code"] . " : $url | ";
+        }
+    }
+    curl_close($ch);
+}
+
+function getURL($text)
+{
+    $parts = explode("ocation:", $text);
+    if (count($parts) < 2) return false;
+
+    $url = substr($parts[1], 0, strpos($parts[1], PHP_EOL));
+
+    return trim($url);
 }
 
 function installer_deps()
